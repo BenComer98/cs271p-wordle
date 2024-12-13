154ca23777a6df90167115e342701ed2e7fc10c2
BenComer98 <benalancomer@gmail.com>
2024-12-13 02:05:45 -0800

    Reverting to port 8080

--- app/gunicorn_config.py
+++ app/gunicorn_config.py
@@ -1,21 +1,10 @@
 import os
 
-# Number of worker processes
 workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
 threads = int(os.environ.get('GUNICORN_THREADS', '2'))
+# timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
 
-# Bind to port with HTTPS
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8443')
-
-# Paths to self-signed certificates
-certfile = '/etc/ssl/certs/selfsigned.crt'
-keyfile = '/etc/ssl/private/selfsigned.key'
-
-# Forward HTTP scheme headers for proper HTTPS routing
 forwarded_allow_ips = '*'
-secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
 
-# Logging
-accesslog = '-'
-errorlog = '-'
-loglevel = 'info'
+secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
