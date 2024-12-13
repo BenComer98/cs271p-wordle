b6311dd59a99ccc0f6b49b36ac34bf12c99f970d
BenComer98 <benalancomer@gmail.com>
2024-12-13 00:20:13 -0800

    Port 8443

--- app/gunicorn_config.py
+++ app/gunicorn_config.py
@@ -6,12 +6,7 @@ threads = int(os.environ.get('GUNICORN_THREADS', '2'))
 
 # Port and bind address
 # Gunicorn will bind to 0.0.0.0:443 to listen for HTTPS traffic
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:443')
-
-# Path to SSL certificate files
-# Make sure you have SSL certificate files ready (cert.pem and key.pem)
-ssl_certfile = os.environ.get('SSL_CERTFILE', '/path/to/your/cert.pem')
-ssl_keyfile = os.environ.get('SSL_KEYFILE', '/path/to/your/key.pem')
+bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8443')
 
 # Forward HTTP scheme headers for proper HTTPS routing
 forwarded_allow_ips = '*'
@@ -21,16 +16,3 @@ secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
 accesslog = '-'
 errorlog = '-'
 loglevel = 'info'
-
-# Bind Gunicorn with SSL
-cert_enabled = os.environ.get('ENABLE_SSL', 'false').lower() == 'true'
-
-if cert_enabled:
-    bind = f"0.0.0.0:443"
-    ssl_options = {
-        'certfile': ssl_certfile,
-        'keyfile': ssl_keyfile
-    }
-    print(f"Using SSL certificates at {ssl_certfile} and {ssl_keyfile}")
-else:
-    print("Running without SSL")
