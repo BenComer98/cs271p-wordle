e745be81aadac25e608eea121175ee22a362b3e8
BenComer98 <benalancomer@gmail.com>
2024-12-13 01:00:20 -0800

    Port 8443

--- app/gunicorn_config.py
+++ app/gunicorn_config.py
@@ -4,15 +4,18 @@ import os
 workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
 threads = int(os.environ.get('GUNICORN_THREADS', '2'))
 
-# Port and bind address
-# Gunicorn will bind to 0.0.0.0:443 to listen for HTTPS traffic
+# Bind to port with HTTPS
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8443')
+
+# Paths to self-signed certificates
+certfile = '/etc/ssl/certs/selfsigned.crt'
+keyfile = '/etc/ssl/private/selfsigned.key'
 
 # Forward HTTP scheme headers for proper HTTPS routing
 forwarded_allow_ips = '*'
 secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
 
-# Enable access logs (optional but recommended)
+# Logging
 accesslog = '-'
 errorlog = '-'
 loglevel = 'info'
